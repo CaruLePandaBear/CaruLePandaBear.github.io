f2a4e666f72e35cd18429ca40612f42f0f566aae
jbmilgrom <jbmilgrom@gmail.com>
2015-01-31 18:36:08 -0500

    testing .load

--- assets/js/javascripts.js
+++ assets/js/javascripts.js
@@ -3,7 +3,7 @@ $(document).ready( function() {
     $("#wedding").on("click", function() {
     	console.log("hello click");
     	console.log($("#loader"));
-        $("#loader").load("services.html #loader", function() {
+        $("#loader").load("services.html #gobble", function() {
   				alert( "Load was performed." );
 			}
 		);
